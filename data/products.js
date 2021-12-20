2a13d0e695ed849858fd36e376dbc37b039e319f
Javier Santoyo <javier.santoyo@yahoo.com>
2021-12-20 01:18:14 -0600

    Crear carpeta desa e incluir Json.

---
+++ data/products.js
@@ -0,0 +1,8 @@
+const product ={
+    id: 01,
+    name: 'chaqueta retro',
+    model: 'chaqueton',
+    marca: 'Louis Vuitron',
+    count: 10,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pellentesque urna sagittis tortor hendrerit consectetur.'
+}
