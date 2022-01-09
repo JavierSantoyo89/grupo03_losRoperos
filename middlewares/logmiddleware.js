44b35267c42f1affc952bc6524b57b6f44117eab
JavierSantoyo89 <javier.santoyo@yahoo.com>
2022-01-08 19:20:08 -0600

    Fixed: links in header

--- middlewares/logmiddleware.js
+++ middlewares/logmiddleware.js
@@ -1,11 +1,12 @@
 const { time } = require('console');
 const fs = require('fs');
 
+//! ---------- middleware que registra el acceso global a un sitio de nuestra web ---------- // 
+
 function logMiddLeware (req, res, next){
     let now = new Date();
-       console.log('hasta aqui jala, por el momento...'+ now);
+       //console.log('hasta aqui jala, por el momento...'+ now);
        fs.appendFileSync('logLosRoperos.txt', 'Visito: ' + req.url + ' ' + now  + '\n');
-        
     next();
 }
 
