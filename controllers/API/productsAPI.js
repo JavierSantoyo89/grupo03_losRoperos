b9d4760218b28f9ab81d62907e6db467035292ad
JavierSantoyo89 <javier.santoyo@yahoo.com>
2022-03-26 20:07:52 -0600

    Normaliza data output

--- controllers/API/productsAPI.js
+++ controllers/API/productsAPI.js
@@ -12,7 +12,11 @@ const productsAPI = {
     ProductsAll: (req,res) =>{
         db.Products.findAll()
             .then(products=>{
-                return res.json(products);
+                return res.status(200).json({
+                    total: products.length,
+                    data: products,
+                    status: 200
+                });
             })
     }
 
