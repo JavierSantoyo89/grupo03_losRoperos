55215b03f85f73387498bc77215d65369c28bfa9
dsalas5 <89938245+dsalas5@users.noreply.github.com>
2022-03-29 12:34:52 -0600

    comence a mejorar el home

--- data/config/config.js
+++ data/config/config.js
@@ -1,7 +1,7 @@
 module.exports ={
   "development": {
     "username": "root",
-    "password": "",
+    "password": "12345",
     "database": "losroperosdb",
     "host": "127.0.0.1",
     "dialect": "mysql"
