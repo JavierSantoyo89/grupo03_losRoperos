ff544ea7e5c644a066b94c2bae970bc95c4cee31
dsalas5 <89938245+dsalas5@users.noreply.github.com>
2022-04-05 12:56:57 -0500

    Lista,Editar y Borrado de Usuarios Listo

--- routers/users.js
+++ routers/users.js
@@ -24,15 +24,18 @@ routerUsers.post('/login',validateLogin, usersController.ProcessLogin);
 
 // ---- Rutas dedicadas a new user's ---- //
 routerUsers.get('/register',usersController.registro);
-routerUsers.post('/register', multerUser, validateRegister ,usersController.processRegister)
-
-
+routerUsers.post('/register', multerUser, validateRegister ,usersController.processRegister);
+routerUsers.get('/list',usersController.listaUsuarios);
+routerUsers.get('/editar/:idUser',usersController.editUser);
+routerUsers.post('/editar/:idUser', multerUser,usersController.updateUser);
+routerUsers.get('/detalle/:idUser', usersController.detailUser);
+routerUsers.post('/borrar/:idUser', usersController.deleteUser);
 // routerUsers.post('/login', validateLogin, usersController.validarUser)
 
 
 // ---- Rutas de API REST ---- //
 routerUsers.get('/all',UsersAPI.UsersAll) // * http://localhost:1689/user/all * //
-routerUsers.get('/:id',UsersAPI.UserId) // * http://localhost:1689/user/{id} * //
+routerUsers.get('idUser',UsersAPI.UserId) // * http://localhost:1689/user/{id} * //
 routerUsers.post('/new_user_api',UsersAPI.new_user_api) // * http://localhost:1689/user/new_user_api * //
 routerUsers.delete('/delete_api/:id',UsersAPI.delete_api) // * http://localhost:1689/user/delete_api/{id} * //
 
