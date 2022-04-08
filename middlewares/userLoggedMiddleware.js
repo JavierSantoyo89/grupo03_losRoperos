24c81a0fa90d6d9e5f3d6e516a8efa39fd26bb5b
Miguel Santamaria <54449523+ElMike1712@users.noreply.github.com>
2022-04-08 03:23:43 -0500

    Update userLoggedMiddleware.js
    Quité un console.log que usé para pruebas

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -3,7 +3,7 @@ let User = require('../data/models/user')
 function userLoggedMiddleware(req, res, next) {
 
     res.locals.isLogged = false;
-    console.log("Ya me ejecuté");
+    
     if(req.session && req.session.userLogged) {
         res.locals.isLogged = true;
 
